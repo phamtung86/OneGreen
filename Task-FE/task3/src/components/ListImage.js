49782e11df06f1a471ad6f1a094d40bf98409ec6
phamtung86 <139665713+phamtung86@users.noreply.github.com>
2025-03-05 16:09:27 +0700

    :[update] update task 3

--- Task-FE/task3/src/components/ListImage.js
+++ Task-FE/task3/src/components/ListImage.js
@@ -1,6 +1,7 @@
 import '../assets/styles/listimage.css'
 import ImageItem from './ImageItem';
 const ListImage = ({ data }) => {
+
     return (
         <div className="list-images">
             {data.map((image, index) => (
